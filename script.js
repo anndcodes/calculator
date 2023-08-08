9a2171f8ae71e1105f70a0f34a7f65e7064783b6
Annd <ssandreia30@gmail.com>
2023-08-08 14:19:13 -0300

    add: calculator buttons and display screen variables

---
+++ script.js
@@ -0,0 +1,8 @@
+// calculator buttons and display screen to use with event listener
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const decimalButton = document.querySelector('[data-decimal]');
+const equalsButton = document.querySelector('[data-equals]');
+const deleteButton = document.querySelector('[data-delete]');
+const clearButton = document.querySelector('[data-clear]');
+const display = document.querySelector('.display');
