adcf468155711fb0b76640528375f4670e31bc26
Annd <ssandreia30@gmail.com>
2023-08-09 11:14:52 -0300

    add: variables to handle operations

--- script.js
+++ script.js
@@ -6,3 +6,10 @@ const equalsButton = document.querySelector('[data-equals]');
 const deleteButton = document.querySelector('[data-delete]');
 const clearButton = document.querySelector('[data-clear]');
 const display = document.querySelector('.display');
+
+// variables to handle operations 
+let firstNum = "";
+let secNum = "";
+let operator = "";
+let currentNum = "0";
+let result = "";
